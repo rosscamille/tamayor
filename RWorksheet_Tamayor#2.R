d7895dec33a3a2f8a6521e70be92168963a6d6d9
Ross Camille Tamayor <tamayorrosscamille@gmail.com>
2023-09-28 04:02:11 +0000

    innitial commit

---
+++ RWorksheet_Tamayor#2.R
@@ -0,0 +1,40 @@
+#1 Create a vector using: operator 
+
+#a.Sequence from -5 to 5
+seqNum <- seq(-5,5)
+seqNum
+
+#b. 
+x <- 1:7).
+x_value <- seq (1:7)
+x_value
+
+#2 Create a vector using seq() function
+
+#a.seq(1, 3, by=0.2)
+seq <- seq(1, 3, by=0.2)
+seq
+
+#
+#
+#
+#
+
+#3 A factory has a census of its workers. There are 50 workers in total.
+
+age <- c (34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27,
+          22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 43, 53, 41, 51, 35,
+          24,33, 41, 53, 40, 18, 44, 38, 41, 48, 27, 39, 19, 30, 61, 54, 58, 26,
+          18)
+
+#a. 3rd element
+age3 <- age[3]
+age3
+
+#b. 2nd and 4th element
+age2 <- age[2]
+age2
+age4 <- age[4]
+age4
+
+#c. all elements but the 4th and 12th element is not included.
